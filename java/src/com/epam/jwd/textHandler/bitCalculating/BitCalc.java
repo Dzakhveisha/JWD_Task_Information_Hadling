325d9bb9357ef98d309eb1df1a7a05597090b195
Dzakhveisha <Dzakhveisha@gmail.com>
2021-05-17 17:32:52 +0300

    fix parsing bit expressions

--- java/src/com/epam/jwd/textHandler/bitCalculating/BitCalc.java
+++ java/src/com/epam/jwd/textHandler/bitCalculating/BitCalc.java
@@ -4,9 +4,12 @@ import com.epam.jwd.textHandler.bitCalculating.expressionImpl.Evaluate;
 
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BitCalc {
     private static BitCalc instance = null;
+    private static final String REGEX = "[-,:’а-яёА-ЯЁ]+|[-,’:a-zA-Z]+";
 
     private BitCalc() {
     }
@@ -23,14 +26,13 @@ public class BitCalc {
     }
 
     public boolean isBitExpression(String text) {
-        if (text.equals("5>>1|6")) {
-            return true;
-        }
-        return false;
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(text);
+        return !matcher.find();
     }
 
     public Long calc(String text) {
-        Expression evaluator = new Evaluate(getReversePolishNotation("5|(1|6)"));
+        Expression evaluator = new Evaluate(getReversePolishNotation(text));
         return evaluator.interpreter(evaluator);
     }
 
@@ -41,15 +43,16 @@ public class BitCalc {
         int i = 0;
         while (i < expression.length()) {
             switch (expression.charAt(i)) {
-                case '~':
+                case '~':{
+                    stack.push(String.valueOf(expression.charAt(i)));
+                }
+                break;
                 case ')': {
                     String op = stack.pop();
                     while (!op.equals("(")) {
                         line.add(op);
                         op = stack.pop();
-                        i--;
                     }
-                    i++;
                 }
                 break;
                 case '(':
@@ -107,8 +110,21 @@ public class BitCalc {
                     }
                     stack.push(String.valueOf(expression.charAt(i)));
                     break;
+                case '^':
+                    while (!stack.empty()) {
+                        String op = stack.pop();
+                        if (op.equals("~") || op.equals(">>")||
+                                op.equals("<<") || op.equals("&")) {
+                            line.add(op);
+                        } else {
+                            stack.push(op);
+                            break;
+                        }
+                    }
+                    stack.push(String.valueOf(expression.charAt(i)));
+                    break;
                 default:
-                    String[] strings = expression.substring(i).split("\\D");
+                    String[] strings = expression.substring(i).split("\\D+");
                     line.add(strings[0]);
                     i += strings[0].length() - 1;
             }
